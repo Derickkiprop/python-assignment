20d47e744a0562c007e6ff700c0a764e775810c2
Derickkiprop <dkiprop312@gmail.com>
2024-11-05 15:16:49 +0300

    added the programming folder and finished the assignment

---
+++ Python Programming/user_input.py
@@ -0,0 +1,7 @@
+name = input ("What is your age? ")
+age = input("What is your age? ")
+location = input("wherer do you live? ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
+
+
